a0fe533e551a728f2b5f9130c7412167d982b6c8
Víctor González <36533965+victorggonzalez@users.noreply.github.com>
2022-06-16 14:35:38 +0200

    test diff functionality

---
+++ compare.py
@@ -0,0 +1,29 @@
+import json
+import sys
+new = sys.argv[1]
+current = sys.argv[2]
+output = { "include": []}
+
+with open(new, "r") as f1:
+    new_file = json.loads(f1.read())
+with open(current, "r") as f2:
+    current_file = json.loads(f2.read())
+
+for current_item in current_file['include']:
+    for new_item in new_file['include']:
+        if new_item['repository'] != current_item['repository']:
+            continue
+        else:
+            if new_item['tag'] != current_item['tag']:
+                print(f"Found difference: {new_item['repository']}")
+                print(new_item['tag'])
+                print(current_item['tag'])
+                print('new_item')
+                print(new_item)
+                output['include'].append(new_item)
+                print(output)
+
+print(f"New output: {output}")
+#new file
+output_file = open("diff-staging.json", "w")
+json.dump(output, output_file, indent=2)
